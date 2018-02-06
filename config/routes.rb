b426d45538afe8c342dbf4926b09528318e79bd6
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 01:15:08 -0200

    Adicionando rota, inserindo links no menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-  	get '/login', to: 'devise/sessions#new'
-  end
-
-  devise_scope :user do
-  	get '/register', to: 'devise/registrations#new'
-  end
+  root 'user#index'
 end
