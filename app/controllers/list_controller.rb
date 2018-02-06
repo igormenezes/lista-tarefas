98346d836bedcdcc306d180b1721f027e9b978c5
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 21:49:20 -0200

    Realizando validacao e insercao dos dados de lista e tarefas

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -4,12 +4,38 @@ class ListController < ApplicationController
 
 	def add
 		begin
+			@error = nil
+			@arrayTasks = []
 			values = params.require(:list).permit :name, :available
+			values[:user_id] = current_user.id
+
 			@list = List.new values
-			if @list.save
-				redirect_to root_url	
+
+			if @list.valid?
+				params[:task].each do | value|
+					@task = Task.new
+					@task.name = value
+					@task.list_id = List.last.id + 1
+					@arrayTasks << @task
+
+					if !@task.valid?
+						@error = true
+					end
+				end
 			else
-				render :show
+				@error = true
+			end
+
+			if !@error.nil?
+				return render :show
+			else
+				@list.save
+
+				@arrayTasks.each do | task |
+					task.save
+				end
+				
+				redirect_to root_url
 			end
 		rescue => e
 			flash[:warning] = "Ocorreu um erro, ao tentar adicionar a lista! Erro: #{e}"
