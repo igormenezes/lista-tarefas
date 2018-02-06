b42efa73633e8580643695bdfc0ca6961dd09fbb
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 21:47:29 -0200

    Realizando alguns ajustes para capturar as tarefas

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 	$('.add-task').click(function(){
 		$('#form-list .last-input').after(
 			"<div class='form-group'>"
-			+ "<label for='task[name][]' class='col-sm-2 control-label'>Task</label>"
+			+ "<label for='task' class='col-sm-2 control-label'>Task</label>"
 			+ "<div class='col-sm-10'>"
-			+	"<input type='text' class='form-control' name='task[name][]' required='true'>"
+			+	"<input type='text' class='form-control' name='task[]' required='true'>"
 			+ "</div>" 
 			+ "</div>"
 		);
