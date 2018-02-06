9c000071b4d85a06eef7fa09b9cd1d1f33439e05
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 19:41:37 -0200

    Realizando a inserção da nvoa lista e retornando erros para a view.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,5 +3,17 @@ class ListController < ApplicationController
 	end
 
 	def add
+		begin
+			values = params.require(:list).permit :name, :available
+			@list = List.new values
+			if @list.save
+				redirect_to root_url	
+			else
+				render :show
+			end
+		rescue => e
+			flash[:warning] = "Ocorreu um erro, ao tentar adicionar a lista! Erro: #{e}"
+	    	return render :show
+		end
 	end
 end
