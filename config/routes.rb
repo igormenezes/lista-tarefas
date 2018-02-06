240c20612120e81f100d5924a87e1ca07142e6c2
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 17:05:33 -0200

    Adicionando as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   root 'user#index'
 
+  get '/list/show' => 'list#show'
+  post 'list/add' => 'list#add'
+
   get '/users', to: redirect(path: 'users/sign_up')
 end
