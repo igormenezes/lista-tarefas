2d18c72b53b3f36a4384352904456370075e4242
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 17:04:56 -0200

    Inserindo inputs de tarefas para as listas no form

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+	$('.add-task').click(function(){
+		$('#form-list .last-input').after(
+			"<div class='form-group'>"
+			+ "<label for='task[]' class='col-sm-2 control-label'>Task</label>"
+			+ "<div class='col-sm-10'>"
+			+	"<input type='text' class='form-control' name='task[]' required='true'>"
+			+ "</div>" 
+			+ "</div>"
+		);
+
+		$('.button-submit').attr('disabled', false);
+	});
+});
+
