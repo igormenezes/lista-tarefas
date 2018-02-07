fd63d80d1581549b96a0170148abdd230b1d485e
Igor Menezes <imigormenezes@gmail.com>
2018-02-07 02:23:26 -0200

    ajustes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
 
   root 'user#index'
 
-  get '/list/show' => 'list#show'
+  get 'list/show' => 'list#show'
   post 'list/add' => 'list#add'
 
   match '*path' => redirect('/'), via: :get
-
-  #get '/users', to: redirect(path: 'users/sign_up')
 end
