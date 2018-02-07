e37b606b0dd9b84ec9a50a77c77a0114cc0e2272
Igor Menezes <imigormenezes@gmail.com>
2018-02-07 03:29:02 -0200

    Adicionando teste para verificar a quantidade de listas ativas por usuario

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe List, type: :model do
 	before(:all) do
 		@user = User.new(email: 'teste@yahoo.com.br', password: '123456')
    		@user.save
-
-		@list = List.new(name: 'teste2', available: 1, user_id: @user.id)
-		@list.save
 	end
 
 	it "add new list and task" do
@@ -19,11 +16,32 @@ RSpec.describe List, type: :model do
 
 	it "add list repeated" do
 		list = List.new(name: 'teste2', available: 1, user_id: @user.id)
+		list.save
+
+		list = List.new(name: 'teste2', available: 1, user_id: @user.id)
 		expect(list.save).to eq false
 	end
 
+	it "show lists by user" do
+		user = User.new(email: 'teste2@yahoo.com.br', password: '123456')
+   		user.save
+
+		list = List.new(name: 'teste1', available: 1, active: 1, user_id: @user.id)
+		list.save
+
+		list = List.new(name: 'teste2', available: 1, active: 0, user_id: @user.id)
+		list.save
+
+		list = List.new(name: 'teste3', available: 1, active: 1, user_id: user.id)
+		list.save
+
+		lists = List.select(:all)
+		.where('lists.user_id = ? AND active = ?', @user.id, 1)
+
+		expect(lists.size).to eq 1
+	end
+
 	after(:all) do
-		@list.destroy
 		@user.destroy
 	end
 end
