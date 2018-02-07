339e1877ed165b3f1b427c08ee16fc06e922e13e
Igor Menezes <imigormenezes@gmail.com>
2018-02-06 22:23:37 -0200

    Realizando alguns ajustes e acrescentando botao remover task

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -2,8 +2,8 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
 	$('.add-task').click(function(){
-		$('#form-list .last-input').after(
-			"<div class='form-group'>"
+		$('#form-list .last-input').before(
+			"<div class='form-group group-task'>"
 			+ "<label for='task' class='col-sm-2 control-label'>Task</label>"
 			+ "<div class='col-sm-10'>"
 			+	"<input type='text' class='form-control' name='task[]' required='true'>"
@@ -11,7 +11,18 @@ $(document).ready(function(){
 			+ "</div>"
 		);
 
+		if ($('.group-task').length) {
+			$('.remove-task').show();
+		}
 		$('.button-submit').attr('disabled', false);
+	});
+
+	$('.remove-task').click(function(){
+		$('.group-task').last().remove();
+
+		if($('.group-task').length == 0){
+			$('.remove-task').hide();
+		}
 	});
 });
 
